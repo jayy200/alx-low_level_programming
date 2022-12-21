b0096aadb8d152df400b065036ea08054498230d
Your Name <you@example.com>
2022-12-20 20:01:39 -0800

    fr

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Description: 'Generates random valid passwords'
+ * Return: 0
+ */
+
+int main(void)
+{
+
+	return (0);
+}
